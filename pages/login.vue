011f0ac3f2217f9da7398f1c124cdc457c7939f4
inzamam31294 <inzamam54321@gmail.com>
2019-05-21 01:18:09 +0500

    21/5/19

--- pages/login.vue
+++ pages/login.vue
@@ -6,32 +6,26 @@
         <form class="loginform">
           <span class="title-login p-b-49">Login</span>
           <div class="login-fields m-b-23">
-            <span class="login-inputs">Username</span>
-            <input
+            <!-- <span class="login-inputs">Username</span> -->
+            <v-text-field
               id="username"
               v-model="username"
-              class="input"
               autofocus="true"
-              placeholder="Enter your Username"
+              prepend-inner-icon="person"
+              label="Username"
               required
-            >
-            <!-- <i class="fas fa-user" /> -->
-            <!-- <span class="focus-input100" data-symbol="&#xf206;" /> -->
-            </input>
+            />
           </div>
           <div class="login-fields m-b-23">
-            <span class="login-inputs">Password</span>
-            <input
+            <!-- <span class="login-inputs">Password</span> -->
+            <v-text-field
               id="password"
               v-model="password"
-              class="input"
               type="password"
-              placeholder="Enter your Password"
+              prepend-inner-icon="lock"
+              label="Password"
               required
-            >
-            <!-- <i class="fas fa-lock" /> -->
-            <!-- <span class="focus-input100" data-symbol="&#xf190;" /> -->
-            </input>
+            />
           </div>
           <div class="text-right p-t-8 p-b-20">
             <router-link to="#" class="forgotpassword">
@@ -104,7 +98,7 @@ div{
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
-    background-image: url("../images/bg-01.jpg")
+    background-image: url("https://picsum.photos/510/300?random");
 }
 .wrap-login{
     width: 500px;
@@ -148,10 +142,6 @@ form{
 .m-b-20{
   margin-bottom: 20px
 }
-.login-fields{
-  border-bottom: 2px solid #d9d9d9;
-  width: 100%
-}
 .m-b-23{
   margin-bottom: 23px;
 }
@@ -161,17 +151,6 @@ form{
   color: #333333;
   line-height: 1.5;
   padding-left: 7px
-}
-.input{
-   font-family: Arial, Helvetica, sans-serif;
-    font-size: 16px;
-    color: #333333;
-    line-height: 1.2;
-    display: block;
-    width: 100%;
-    height: 55px;
-    background: transparent;
-    padding: 0 7px 0 43px;
 }
 .text-right{
   text-align: right;
@@ -254,77 +233,7 @@ form{
   left: 0;
 }
 
-.fas{
-  color: #adadad;
-}
-
 /*---------------------------------------------*/
-input{
-  border: none;
-  outline: none
-}
-/* i {
-  position: absolute;
-  display: block;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  pointer-events: none;
-}
-
-i::after {
-  content: attr(fas);
-  font-family: Material-Design-Iconic-Font;
-  font-weight:normal;
-  color: #adadad;
-  font-size: 22px;
-
-  display: -webkit-box;
-  display: -webkit-flex;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  height: calc(100% - 20px);
-  bottom: 0;
-  left: 0;
-  padding-left: 13px;
-  padding-top: 3px;
-}
-
-i::before {
-  content: "";
-  display: block;
-  position: absolute;
-  bottom: -2px;
-  left: 0;
-  width: 0;
-  height: 2px;
-  background: #7f7f7f;
-  -webkit-transition: all 0.4s;
-  -o-transition: all 0.4s;
-  -moz-transition: all 0.4s;
-  transition: all 0.4s;
-}
-
-.input:focus + i::before {
-  width: 100%;
-}
-
-.has-val.input + i::before {
-  width: 100%;
-}
-
-.input:focus + i::after {
-  color: #a64bf4;
-}
-
-.has-val.input + i::after {
-  color: #a64bf4;
-} */
 .signup-txt{
   font-family: Arial, Helvetica, sans-serif;
   font-size: 14px;
@@ -364,3 +273,4 @@ i::before {
 
 // http://markinternational.info/data/out/1/218520820-1440-wallpapers.jpg
 // https://backgrounddownload.com/wp-content/uploads/2018/09/colourful-abstract-background-paintings-7.jpg
+//  url("../images/bg-01.jpg"),
